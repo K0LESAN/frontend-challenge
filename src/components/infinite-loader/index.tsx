f1ad6bcfbbcdff33689b9c856847a87c3cdb90c7
K0LESAN <k0lesan2k00@gmail.com>
2025-01-17 22:18:29 +0300

    feat: add infinite load cats

--- src/components/infinite-loader/index.tsx
+++ src/components/infinite-loader/index.tsx
@@ -1,7 +1,36 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
 import styles from './index.module.css';
 
-export function InfiniteLoader() {
-  return <div className={styles.loader}>... загружаем еще котиков ...</div>;
+export function InfiniteLoader({
+  callback
+}: {
+  callback: IntersectionObserverCallback;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callback, {
+      threshold: 0.65
+    });
+    let observableRef: HTMLDivElement | null = null;
+
+    if (ref.current) {
+      observableRef = ref.current;
+      observer.observe(observableRef);
+    }
+
+    return () => {
+      if (observableRef) {
+        observer.unobserve(observableRef);
+      }
+    };
+  }, [callback]);
+
+  return (
+    <div ref={ref} className={styles.loader}>
+      ... загружаем еще котиков ...
+    </div>
+  );
 }
