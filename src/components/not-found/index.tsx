b7df0541ae4dc86dcf5473517ed0f1c118910209
K0LESAN <k0lesan2k00@gmail.com>
2025-01-17 18:09:21 +0300

    feat: add not-found

---
+++ src/components/not-found/index.tsx
@@ -0,0 +1,15 @@
+import Container from '../container';
+import styles from './index.module.css';
+
+export default async function NotFound() {
+  return (
+    <Container>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Упс...</h2>
+        <p className={styles.descrption}>
+          Такой страницы с котиками не найдено
+        </p>
+      </div>
+    </Container>
+  );
+}
