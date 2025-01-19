6afdbb438da7cec8b3e0bb8d5fd36ea018258822
K0LESAN <k0lesan2k00@gmail.com>
2025-01-19 19:37:04 +0300

    feat: add placeholder for loading and error images

--- src/components/cat/index.tsx
+++ src/components/cat/index.tsx
@@ -1,20 +1,43 @@
 'use client';
 
 import Image from 'next/image';
+
+import { useState } from 'react';
+import UnexpectedErrorCat from '@/assets/unexpected-error.jpg';
 import Heart from '../heart';
+import Loader from '../loader';
 import styles from './index.module.css';
 
 export default function Cat({ id }: { id: string }) {
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   return (
     <div className={styles.block}>
-      <Image
-        src={`https://cataas.com/cat/${id}`}
-        alt=''
-        width={225}
-        height={225}
-        className={styles.cat}
-      />
-      <Heart id={id} />
+      {isLoading && !isError && <Loader />}
+      {isError && (
+        <Image
+          src={UnexpectedErrorCat}
+          alt='Unexpected error'
+          className={styles.cat}
+        />
+      )}
+      {!isError && (
+        <Image
+          src={`https://cataas.com/cat/${id}`}
+          alt=''
+          width={225}
+          height={225}
+          className={`${styles.cat} ${isLoading ? styles.cat_hidden : ''}`}
+          onLoad={() => {
+            setIsLoading(false);
+          }}
+          onError={() => {
+            setIsError(true);
+          }}
+        />
+      )}
+      {!isLoading && !isError && <Heart id={id} />}
     </div>
   );
 }
